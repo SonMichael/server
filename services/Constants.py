50485c1afbf940e76ba184fa57b14f4a62795b18
sonle <sonle@les-MacBook-Pro.local>
2022-08-03 19:06:50 +0700

    add comment

--- services/Constants.py
+++ services/Constants.py
@@ -5,10 +5,10 @@ IMAGE_WIDTH_V2 = 32
 IMAGE_HEIGHT_V2 = 32
 API_VERSION_V1 = 'v1'
 CLASSES_V2 = {
-    1: 0,
-    4: 1,
-    5: 2,
-    6: 3,
+    1: 0, # user_id = 1; 0 = Định
+    4: 1, # user_id = 4; 1 = Mạnh
+    5: 2, # user_id = 5; 2 = Thái
+    6: 3, # user_id = 6; 3 = Tuấn
 }
 CONFIGS = {
     'API_VERSION': 'API_VERSION'
